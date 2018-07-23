cc721ccbebb7ce90c89f86ed95a5376095033fbb
Dmytro Klimchuk <Dmytro.Klimchuk@gmail.com>
2018-07-23 23:04:50 +0300

    materialize to project, created model, started header, body, footer markup, created base components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BookComponent } from './book/book.component';
+import { BookListComponent } from './book/book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BookComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ BookService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/book/book.model.ts
@@ -0,0 +1,31 @@
+// - Прізвище (обов'язкове поле)
+// - ім'я (обов'язкове поле)
+// - країна
+// - місто
+// - email (обов'язкове поле)
+// - номер телефону (обов'язкове поле)
+// - місце роботи (назва компанії)
+
+interface IBook {
+    surname: string;
+    name: string;
+    country: string;
+    city: string;
+    email: string;
+    mobile: number;
+    work_place: string;
+}
+
+export class Book implements IBook {
+
+    constructor (
+        public surname: string,
+        public name: string,
+        public country: string,
+        public city: string,
+        public email: string,
+        public mobile: number,
+        public work_place: string
+    ) {}
+
+}
